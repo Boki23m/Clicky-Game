3219e35912a3908b1cb33a9a85ef5d3da4a88fe5
Boki23m <boki23usa@yahoo.com>
2019-04-23 22:34:18 -0700

    Updates

---
+++ precache-manifest.14b4bd57d2ea107b7a9a7180e68c7f2b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e82c781ae126aa3f1aecaf2f4391bfcd",
+    "url": "/Clicky-Game/index.html"
+  },
+  {
+    "revision": "17e5d702f6a3d93fb693",
+    "url": "/Clicky-Game/static/css/main.b2f20610.chunk.css"
+  },
+  {
+    "revision": "dd0ed99a380f8d225736",
+    "url": "/Clicky-Game/static/js/2.884c28c3.chunk.js"
+  },
+  {
+    "revision": "17e5d702f6a3d93fb693",
+    "url": "/Clicky-Game/static/js/main.0f1a8558.chunk.js"
+  },
+  {
+    "revision": "fab82da4d9420c34bc57",
+    "url": "/Clicky-Game/static/js/runtime~main.012bace8.js"
+  }
+]);
